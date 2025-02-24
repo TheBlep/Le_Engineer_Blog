6a8a5d7e5f58e4d94e619657fa9ddfdd57f25511
TheBlep <soshire@protonmail.com>
2025-02-24 11:06:44 +0100

    edit(build): update settings.py, urls.py and veiws.py to  bind with heroku

--- codestar/blog/views.py
+++ codestar/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-	return HttpResponse("hello world!")
+def index(request):
+	return HttpResponse("Hello world!")
