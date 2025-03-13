477dc4b98bb903fc46521f26bb56beadc5c59670
TheBlep <soshire@protonmail.com>
2025-03-13 14:56:58 +0100

    edit(contant): edited collbaeration page content

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,13 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# if DEBUG:
-#     SECRET_KEY = 'mysecretkey'
-# else:
-#     SECRET_KEY = os.environ['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
